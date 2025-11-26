19dd8b87e156185c83f16fbe2d6f9a5a8eb9540f
Richmond Constante <r.constante.dev@gmail.com>
2025-11-26 12:01:24 +0800

    Still fixing the IOT and Camera

--- camera_system/__init__.py
+++ camera_system/__init__.py
@@ -6,19 +6,11 @@ Provides camera detection and ML model integration for Smart Classroom
 from .camera_detector import CameraDetector, CameraStream, get_system_info
 from .emotion_detector import EmotionDetector
 from .yolo_face_detector import YOLOFaceDetector
-from .ml_models import (
-    YOLODetector,
-    CNNClassifier,
-    ModelEnsemble
-)
 
 __all__ = [
     'CameraDetector',
     'CameraStream',
     'get_system_info',
     'EmotionDetector',
-    'YOLOFaceDetector',
-    'YOLODetector',
-    'CNNClassifier',
-    'ModelEnsemble'
+    'YOLOFaceDetector'
 ]
